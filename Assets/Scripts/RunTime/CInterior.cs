0cd9ab5fad5d72ee840efc02bd8ad19b05ca6f49
Ryw <yyw980312@gmail.com>
2026-01-30 17:56:33 +0900

    기본적인 정보를 담은 인테리어 프리팹을 위한 스크립트와 프리팹.

--- Assets/Scripts/RunTime/CInterior.cs
+++ Assets/Scripts/RunTime/CInterior.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+using System;
 using UnityEngine;
 
 
@@ -10,18 +10,49 @@ using UnityEngine;
 
 public class CInterior : MonoBehaviour
 {
+    public enum EInterState
+    {
+        Offline,
+        Online,
+        Disabled,
+        Count
+    }
+
+    [Flags]
+    public enum EEquipmentPosition
+    {
+        UpLeft = 1 << 0,
+        UpRight = 1 << 1,
+        RightUp = 1 << 2,
+        RightDown = 1 << 3,
+        DownLeft = 1 << 4,
+        DownRight = 1 << 5,
+        LeftUp = 1 << 6,
+        LeftDown = 1 << 7,
+    }
+
     #region 인스펙터
     [Header("메인 이미지")]
     [SerializeField] private SpriteRenderer _mainSpriteRenderer;
     [SerializeField] private Sprite _mainSprite;
 
     [Header("글로우 이미지")]
+    [SerializeField] private bool _isUseGlow = true;
     [SerializeField] private SpriteRenderer _glowSpriteRenderer;
     [SerializeField] private Sprite[] _glowSprites;
+    // 0 - 자리에 없
+    // 1 - 자리에 있
+    // 2 - 작동 불가능
+
+    [Header("사이즈 (방처럼 2, 4만 존재)")]
+    [SerializeField] private int _size;
+
+    [Header("장비 위치")]
+    [SerializeField] private EEquipmentPosition _equipmentPosition;
     #endregion
 
     #region 내부 변수
-
+    private EInterState _currentState = EInterState.Offline;
     #endregion
 
     void Awake()
@@ -31,20 +62,43 @@ public class CInterior : MonoBehaviour
         if (_mainSprite == null)
             Debug.LogWarning($"At {gameObject.name} : _mainSprite == null");
 
-        if (_glowSpriteRenderer == null)
-            Debug.LogWarning($"At {gameObject.name} : _glowSpriteRenderer == null");
-        if (_glowSprites == null || _glowSprites.Length < 3)
-            Debug.LogWarning($"At {gameObject.name} : _glowSprites == null || _glowSprites.Length < 3");
+        if (_isUseGlow == true)
+        {
+            if (_glowSpriteRenderer == null)
+                Debug.LogWarning($"At {gameObject.name} : _glowSpriteRenderer == null");
+            if (_glowSprites == null || _glowSprites.Length < 3)
+                Debug.LogWarning($"At {gameObject.name} : _glowSprites == null || _glowSprites.Length < 3");
+        }
+
+        if (_size != 2 && _size != 4)
+        {
+            Debug.LogWarning($"At {gameObject.name} : 방의 크기는 무조건 2 아니면 4이여야 합니다. 일단은");
+        }
+
     }
 
     void Start()
     {
         _mainSpriteRenderer.sprite = _mainSprite;
-        _glowSpriteRenderer.sprite = _glowSprites[1];
+        if (_isUseGlow == true)
+        {
+            _glowSpriteRenderer.sprite = _glowSprites[0];
+        }
     }
 
     void Update()
     {
-        
+
+    }
+
+    public void ChangeState(EInterState state)
+    {
+        if (!_isUseGlow) return; // 점등 효과가 없는 것들도 있더라..
+
+        if (_currentState != state)
+        {
+            _currentState = state;
+            _glowSpriteRenderer.sprite = _glowSprites[(int)_currentState];
+        }
     }
 }
--- Assets/Scripts/RunTime/CSystem.cs
+++ Assets/Scripts/RunTime/CSystem.cs
@@ -21,6 +21,10 @@ ui
 
 public class CSystem : MonoBehaviour
 {
+    public enum ESystemType
+    {
+
+    }
     #region 인스펙터
     // 그려낼 프리팹
     [Header("인테리어 프리팹")]
@@ -34,7 +38,10 @@ public class CSystem : MonoBehaviour
 
     void Awake()
     {
-
+        // 인테리어 프리팹은 방의 구조에 따라서 다른걸 가져온다.
+        // 문의 위치에 따라서 다른 프리팹을 가져와야한다.
+        // enumFlag로 방과 비교해 걸리는게 없어야 가능하다.
+        // 시스템의 종류와 방의 정보를 갖고 설치를 시도한다.
     }
 
     void Start()
